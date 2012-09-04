dbffedd35c1b562da44303c7f67e7de072a8eabf
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-09-04 18:40:34 -0300

    Post search template.

---
+++ admin/post-search.php
@@ -0,0 +1,5 @@
+<?php setup_postdata( $post ); ?>
+<div class="post-search" id="post-search-<?php the_ID(); ?>">
+    <h4><?php the_title(); ?></h4>
+    <span class="excerpt"><?php the_excerpt(); ?></span>
+</div>
