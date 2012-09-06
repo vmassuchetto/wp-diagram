b85efa31e47707e87cec71855833afca0a70c356
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-09-06 08:03:10 -0300

    Fixing date selection on position update.

--- admin/position.php
+++ admin/position.php
@@ -1,3 +1,19 @@
+<?php
+    $current_schedule = false;
+    $schedule = $wp_diagram->get_schedule( $position['id'] );
+
+    if ( ! empty( $date ) ) {
+        foreach ( $schedule as $s ) {
+            if ( $date == $s->date )
+                $current_schedule = $s;
+        }
+    } else {
+        $date = false;
+    }
+    $posts = false;
+    if ( ! empty( $current_schedule->posts ) )
+        $posts = json_decode( $current_schedule->posts );
+?>
 <div class="postbox" id="position-<?php echo $position['id']; ?>">
 <h3><label for="link_name"><?php echo $position['name']; ?></label></h3>
 
@@ -10,17 +26,17 @@
         name="position-<?php echo $position['id']; ?>-add-post"
         value="<?php _e( 'Search by post title', 'wp_diagram' ); ?>" />
 
-    <?php if ( $schedule = $wp_diagram->get_schedule( $position['id'] ) ) : $i = 0; ?>
+    <?php if ( $schedule ) : ?>
         <?php _e( 'Schedule', 'wp_diagram' ); ?>&nbsp;
         <select id="position-<?php echo $position['id']; ?>-select-schedule"
             class="position-select-schedule"
             name="position-<?php echo $position['id']; ?>-schedule">
-                <?php foreach( $schedule as $s ) : $i++; ?>
-                    <option value="<?php echo $s->date; ?>"
-                        <?php echo( (! isset( $date ) && $i == 1) || ( isset( $date ) && $date == $s->date ) ) ? 'selected' : ''; ?>>
-                        <?php echo mysql2date('j M Y H:i', $s->date ); ?>
-                    </option>
-                <?php endforeach; ?>
+            <?php foreach( $schedule as $s ) : $i++; ?>
+                <option value="<?php echo $s->date; ?>"
+                    <?php echo ( $date == $s->date ) ? 'selected' : ''; ?>>
+                    <?php echo mysql2date('j M Y H:i', $s->date ); ?>
+                </option>
+            <?php endforeach; ?>
         </select>
     <?php endif; ?>
 
